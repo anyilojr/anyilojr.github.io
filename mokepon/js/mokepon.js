40e96ecf40ea62e6908f58867794e8320b167371
Anyilo Rangel <anyilojoserangelarias@gmail.com>
2022-07-21 23:57:00 -0500

    Modificaciones del día de hoy

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,30 +1,43 @@
+let ataqueJugador 
 function iniciarJuego(){
   let botonMascotaJugador = document.getElementById("boton-mascotas");
   botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
 
 }
 
-function seleccionarMascotaJugador(){
+function seleccionarMascotaJugador()
+{
   let inputHipodoge = document.getElementById("hipodoge");
   let inputCapipepo = document.getElementById("capipepo");
   let inputRatigueya = document.getElementById("ratigueya");
-  let inputLangostelvis = document.getElementById("langostelvis");
-  let inputTupalma = document.getElementById("tupalma");
-  let inputPydos = document.getElementById("pydos");
+  let spanMascotaJugador = document.getElementById("mascota-jugador");
 
   if(inputHipodoge.checked){
-    alert("Seleccionaste a Hipodoge");
+    spanMascotaJugador.innerHTML = "Hipodoge";
   }else if(inputCapipepo.checked){
-    alert("Seleccionaste a Capipepo");
+    spanMascotaJugador.innerHTML = "Capipepo";
   }else if(inputRatigueya.checked){
-    alert("Seleccionaste a Ratigueya");
-  }else if(inputLangostelvis.checked){
-    alert("Seleccionaste a Langostelvis");
-  }else if(inputTupalma.checked){
-    alert("Seleccionaste a Tupalma");
-  }else if(inputPydos.checked){
-    alert("Seleccionaste a Pydos");
+    spanMascotaJugador.innerHTML = "Ratigueya";
+  }else{
+    alert("Selecciona una Mascota");
+  }
+  seleccionarMascotaEnemigo()
+}
+
+function seleccionarMascotaEnemigo(){
+  let ataqueAleatorio = aleatorio(1,3);
+  let spanMascotaEnemigo = document.getElementById("mascota-enemigo")
+
+  if(ataqueAleatorio == 1){
+    spanMascotaEnemigo.innerHTML = "Hipodoge"
+  } else if(ataqueAleatorio == 2){
+    spanMascotaEnemigo.innerHTML = "Capipepo"
+  } else {
+    spanMascotaEnemigo.innerHTML = "Ratigueya"
   }
 }
 
+function aleatorio(min, max){
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
 window.addEventListener("load", iniciarJuego)
