0793fbf60d36ad93b085beb49f4a1f065ec38311
Anyilo Rangel <anyilojoserangelarias@gmail.com>
2022-07-21 22:21:21 -0500

    Javascript

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,1 +1,30 @@
-alert("Hola mundo");
+function iniciarJuego(){
+  let botonMascotaJugador = document.getElementById("boton-mascotas");
+  botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
+
+}
+
+function seleccionarMascotaJugador(){
+  let inputHipodoge = document.getElementById("hipodoge");
+  let inputCapipepo = document.getElementById("capipepo");
+  let inputRatigueya = document.getElementById("ratigueya");
+  let inputLangostelvis = document.getElementById("langostelvis");
+  let inputTupalma = document.getElementById("tupalma");
+  let inputPydos = document.getElementById("pydos");
+
+  if(inputHipodoge.checked){
+    alert("Seleccionaste a Hipodoge");
+  }else if(inputCapipepo.checked){
+    alert("Seleccionaste a Capipepo");
+  }else if(inputRatigueya.checked){
+    alert("Seleccionaste a Ratigueya");
+  }else if(inputLangostelvis.checked){
+    alert("Seleccionaste a Langostelvis");
+  }else if(inputTupalma.checked){
+    alert("Seleccionaste a Tupalma");
+  }else if(inputPydos.checked){
+    alert("Seleccionaste a Pydos");
+  }
+}
+
+window.addEventListener("load", iniciarJuego)
