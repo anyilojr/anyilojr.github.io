e940c25dc04fd74796fa0935088c2a6f04a1e33a
Anyilo Rangel <anyilojoserangelarias@gmail.com>
2022-07-27 19:11:13 -0500

    express js y primer hola mundo

--- mokepon/index.js
+++ mokepon/index.js
@@ -1,1 +1,15 @@
-console.log("Hola Node")
+// importamos expressjs para usarlo en el proyecto
+const express = require("express")
+// almacena la aplicacion
+const app = express()
+// escucha la peticion del cliente por medio de un puerto 
+
+// cuando reciba una peticion en la url raiz, responda hola
+app.get("/", (req, res) => {
+  res.send("Hola")
+})
+
+// escuche continuamente en el puerto 8080 las peticiones de los clientes, para que responda siempre
+app.listen(8080, () =>{
+  console.log("Servidor funcionando")
+})
