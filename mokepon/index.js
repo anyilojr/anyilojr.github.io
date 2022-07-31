14a94a28e883e88353f720a318200bec2ba758e7
Anyilo Rangel <anyilojoserangelarias@gmail.com>
2022-07-31 15:16:34 -0500

    Transmisión de coordenadas

--- mokepon/index.js
+++ mokepon/index.js
@@ -1,58 +1,3 @@
-// // importamos expressjs para usarlo en el proyecto
-// const express = require("express")
-// const cors = require("cors")
-// // almacena la aplicacion
-// const app = express()
-
-// app.use(cors())
-// app.use(express.json())
-
-// const jugadores = []
-
-// class Jugador{
-//   consructor(id){
-//     this.id = id
-//   }
-//   asignarMokepon(mokepon){
-//     this.mokepon = mokepon
-//   }
-// }
-// class Mokepon {
-//   constructor(nombre){
-//     this.nombre = nombre
-//   }
-// }
-// // escucha la peticion del cliente por medio de un puerto 
-
-// // cuando reciba una peticion en la url raiz, responda hola
-// app.get("/unirse", (req, res) => {
-//   const id = `${Math.random()}`
-//   const jugador = new Jugador(id)
-
-//   jugadores.push(jugador)
-
-//   res.setHeader("Access-Control-Allow-Origin", "*")
-//   res.send(id)
-// })
-
-// app.post("/mokepon/:jugadorId", (req, res) =>{
-//   const jugadorId = req.params.jugadorId || ""
-//   const nombre = req.body.mokepon || ""
-//   const mokepon = new Mokepon(nombre)
-//   const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
-  
-//   if(jugadorIndex >= 0){
-//     jugadores[jugadorIndex].asignarMokepon(mokepon)
-//   }
-//   console.log(jugadores)
-//   console.log(jugadorId)
-//   res.end()
-// })
-
-// // escuche continuamente en el puerto 8080 las peticiones de los clientes, para que responda siempre
-// app.listen(8080, () =>{
-//   console.log("Servidor funcionando")
-// })
 const express = require("express")
 const cors = require("cors")
 
@@ -71,6 +16,11 @@ class Jugador {
   asignarMokepon(mokepon) {
     this.mokepon = mokepon
   }
+  actualizarPosicion(x, y){
+    this.x = x 
+    this.y = y
+  }
+
 }
 
 class Mokepon {
@@ -91,7 +41,7 @@ app.get("/unirse", (req, res) => {
   res.send(id)
 })
 
-app.post("mokepon/:jugadorId", (req, res) => {
+app.post("/mokepon/:jugadorId", (req, res) => {
   const jugadorId = req.params.jugadorId || ""
   const nombre = req.body.mokepon || ""
   const mokepon = new Mokepon(nombre)
@@ -104,6 +54,18 @@ app.post("mokepon/:jugadorId", (req, res) => {
   
   console.log(jugadores)
   console.log(jugadorId)
+  res.end()
+})
+
+app.post("/mokepon/:jugadorId/posicion", (req, res) => {
+  const jugadorId = req.params.jugadorId || ""
+  const x = req.body.x || 0
+  const y = req.body.y || 0
+  const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
+
+  if (jugadorIndex >= 0) {
+    jugadores[jugadorIndex].asignarMokepon(x, y)
+  }
   res.end()
 })
 
